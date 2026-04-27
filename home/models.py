1e33fa05c892ec4af274d0fe4904241b0e4205a7
Nasreen Shah <nasreenshah704@gmail.com>
2026-04-27 18:08:41 +0530

    Add Ctaegory in add product form and filter on shop page

--- home/models.py
+++ home/models.py
@@ -6,10 +6,17 @@ class Contact(models.Model):
     email = models.CharField(max_length=122)
     description = models.TextField()
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+        
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     image = models.ImageField(upload_to='img/')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
     description = models.TextField()
 
     def __str__(self):
